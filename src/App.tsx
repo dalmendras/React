6e301ebd94d36704c833412e799775147044900e
dalmendras <dalmendrasr@hotmail.com>
2024-09-06 14:09:55 +0000

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import './App.css'
+import "./App.css";
 
 export default function App() {
-  return (
-    <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
-    </main>
-  )
+  return <main>React ⚛️ + Vite ⚡ + Replit Hello</main>;
 }
